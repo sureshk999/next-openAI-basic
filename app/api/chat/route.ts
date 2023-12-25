1bec6cf97c81b3d878aef0ab41466f09b4419f20
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-12-25 19:33:02 +1000

    add more logging

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -25,9 +25,19 @@ export async function POST(req: Request) {
 
     return new StreamingTextResponse(stream);
   } catch (error) {
-    console.error("Error in POST /api/chat: ", error); // Detailed error logging
-    return new Response(JSON.stringify({ message: "Internal Server Error" }), {
-      status: 500,
+    console.error("Error in POST /api/chat: ", error);
+
+    let status = 500;
+    let message = "Internal Server Error";
+
+    // Check if the error is due to authentication (like an invalid API key)
+    if (error.response && error.response.status === 401) {
+      status = 401;
+      message = "Invalid API Key";
+    }
+
+    return new Response(JSON.stringify({ message }), {
+      status,
       headers: {
         'Content-Type': 'application/json',
       },
