4b9a298b62751580ab078147547201e2e3fae62c
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-12-25 14:32:01 +1000

    initial commit

---
+++ app/components/Chat.js
@@ -0,0 +1,28 @@
+'use client';
+ 
+import { useChat } from 'ai/react';
+ 
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    api: '/pages/api/chat',
+  });
+ 
+  return (
+    <div>
+      {messages.map(m => (
+        <div key={m.id}>
+          {m.role === 'user' ? 'User: ' : 'AI: '}
+          {m.content}
+        </div>
+      ))}
+ 
+      <form onSubmit={handleSubmit}>
+        <label>
+          Say something...
+          <input value={input} onChange={handleInputChange} />
+        </label>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+}
---
+++ app/pages/api/singleprompt/route.ts
@@ -0,0 +1,42 @@
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+ 
+// Create an OpenAI API client (that's edge friendly!)
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+ 
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge';
+ 
+export async function POST(req: Request) {
+  // Extract the `prompt` from the body of the request
+  const { prompt } = await req.json();
+ 
+  // Ask OpenAI for a streaming completion given the prompt
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo-1106",
+        messages: [
+            {
+                role: "system",
+                content: 'be a friendly and funny chat bot',
+            },
+            {
+                role: "user",
+                content: prompt // string that the user passes in
+            }
+        ],
+        temperature: 0.2,
+        max_tokens: 100,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        stream:true,
+  });
+ 
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+ 
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
