641a19753d253bbd1dcc7076503dce108ab1360b
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-12-25 14:59:41 +1000

    move API

--- app/pages/api/chat/route.ts
+++ app/pages/api/chat/route.ts
@@ -1,13 +1,14 @@
 import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
  
+// IMPORTANT! Set the runtime to edge
+export const runtime = 'edge';
+
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
  
-// IMPORTANT! Set the runtime to edge
-export const runtime = 'edge';
  
 export async function POST(req: Request) {
   const { messages } = await req.json();
