4c216f8056c778ff3277872de86bf01eb1c945c6
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-12-25 15:24:18 +1000

    add error handling

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,27 +1,36 @@
 import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
  
-// IMPORTANT! Set the runtime to edge
 export const runtime = 'edge';
 
-// Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
  
- 
 export async function POST(req: Request) {
-  const { messages } = await req.json();
+  try {
+    const { messages } = await req.json();
+
+    console.log("Received messages: ", messages); // Log received messages
  
-  // Ask OpenAI for a streaming chat completion given the prompt
-  const response = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages,
-  });
- 
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
-  // Respond with the stream
-  return new StreamingTextResponse(stream);
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      stream: true,
+      messages,
+    });
+
+    console.log("OpenAI response: ", response); // Log OpenAI response
+
+    const stream = OpenAIStream(response);
+
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    console.error("Error in POST /api/chat: ", error); // Detailed error logging
+    return new Response(JSON.stringify({ message: "Internal Server Error" }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
 }
